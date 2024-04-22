0adbd3c0369936f1665061baf37b8dd8aa2dac21
camoma <carmoma9@gmail.com>
2024-04-22 10:19:51 +0200

    Added PERMANOVA function controlling for other variables

--- make_permanova.R
+++ make_permanova.R
@@ -18,3 +18,36 @@ for(i in names(all_phyloseq)){
   }, phobj, exclude_vars, SEED)
   names(permaresults[[i]]) <- dtypes
 }
+
+
+### ADONIS with multiple variables
+permaformulas <- c(
+  "braydist ~ Condition + Sexo",
+  "braydist ~ Condition + BMI_log",
+  "braydist ~ Condition + Edad_log",
+  "braydist ~ Condition + IPAQ_act_fisica",
+  "braydist ~ Condition + Mediterranean_diet_adherence2",
+  "braydist ~ Condition + BMI_log + Sexo + Edad_log",
+  "braydist ~ Condition + BMI_log + Sexo + Edad_log + IPAQ_act_fisica",
+  "braydist ~ Condition + BMI_log + Sexo + Edad_log + Mediterranean_diet_adherence2",
+  "braydist ~ Condition + BMI_log + Sexo + Edad_log + IPAQ_act_fisica + Mediterranean_diet_adherence2"
+)
+
+permaresults_mult <- list()
+for(i in names(all_phyloseq)){
+  cat("Doing PERMANOVA of: ", i)
+  phobj <- all_phyloseq[[i]]
+  phobj <- updatePsWithLogs(phobj, c("Edad", "BMI"))
+  permaresults_mult[[i]] <- lapply(dtypes, FUN=function(dd, phobj, exclude_vars, SEED){
+    oname <- paste0(outdir, "permanova_resultsMult_", i, "_", dd, ".tsv")
+    makePermanovaFormulas(phobj,
+                  permaformulas,
+                  dist_method = dd, 
+                  seed = SEED, 
+                  outname = oname) 
+  }, phobj, exclude_vars, SEED)
+  names(permaresults_mult[[i]]) <- dtypes
+}
+save(permaresults_mult, file = paste0(outdir, "PERMANOVA_MULT.RData"))
+
+mm <- permaresults_mult$remove_tanda2_rarefied_min$bray$modelos
--- metagenomics_core_functions.R
+++ metagenomics_core_functions.R
@@ -1314,6 +1314,33 @@ makePermanova <- function(phobj, dist_method = "bray", seed = 123,
   
 }
 
+
+makePermanovaFormulas <- function(phobj, formulas, dist_method = "bray", seed = 123,
+                                  outname = "permanovas_mult.tsv"){
+  ## From https://deneflab.github.io/MicrobeMiseq/demos/mothur_2_phyloseq.html#permanova
+  library(stringi)
+  
+  meta <- sample_data(phobj)
+  braydist <- phyloseq::distance(phobj, method = dist_method)
+  sampledf <- data.frame(sample_data(phobj))
+  names(sampledf) <- stri_trans_general(str = names(sampledf), id = "Latin-ASCII") %>% 
+    gsub(" ", "_", .)
+  
+  modelos <- list()
+  res <- data.frame()
+  for(form in formulas){
+    set.seed(seed)
+    mod1 <- adonis2(as.formula(form), data = sampledf, na.action=na.exclude)
+    res <-rbind(res, adonis2table(mod1))
+    modelos[[form]] <- mod1
+  }
+  res <- res %>% dplyr::arrange(P)
+  res$padj <- p.adjust(res$P, method="BH")
+  write_tsv(res, file=outname)
+  return(list(res=res, modelos=modelos))
+  
+}
+
 makeBetaDispTests<- function(phobj, dist_method = "bray", 
                   exclude_vars = c("sampleID"), 
                   outname){
